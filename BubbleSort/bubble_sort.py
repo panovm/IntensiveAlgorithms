a16c49536e9537a6ff9299413dbb19da15d98657
Mikhail Panov <54293295+panovm@users.noreply.github.com>
2023-08-18 18:21:38 +0300

    solved bubble_sort.py

--- BubbleSort/bubble_sort.py
+++ BubbleSort/bubble_sort.py
@@ -11,8 +11,12 @@ def bubble_sort(array: List[int]) -> List[int]:
     Returns:
         List[int]: sorted array
     """
+    for j in range(len(array) - 1):
+        for i in range(len(array) - 1):
+            if array[i] > array[i+1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
 
-    return []
+    return array
 
 
 # https://leetcode.com/problems/sort-colors/description/
