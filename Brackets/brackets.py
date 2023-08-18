15e331e4736cbfa1db2f39c76ea2d4e39577ff8a
Mikhail Panov <54293295+panovm@users.noreply.github.com>
2023-08-18 21:28:39 +0300

    solved brackets.py

--- Brackets/brackets.py
+++ Brackets/brackets.py
@@ -1,17 +1,19 @@
 def are_brackets_correct(string: str) -> bool:
-    """
-    Accepts a string consisting only of opening and closing parentheses as input,
-    and checks whether this string is correct.
-    An empty string (absence of parentheses) is considered correct.
-
-    Args:
-        string (str): string to check
-
-    Returns:
-        bool: are parentheses correct
-    """
-
-    return False
+    D = []
+    for bracket in string:
+        if bracket in ['(', '[', '{']:
+            D.append(bracket)
+        else:
+            if len(D) == 0:
+                return False
+            last_opened = D.pop()
+            if (last_opened == '(' and bracket != ')') \
+               or (last_opened == '[' and bracket != ']') \
+               or (last_opened == '{' and bracket != '}') :
+                return False
+    if len(D):
+        return False
+    return True
 
 
 # https://leetcode.com/problems/valid-parentheses/
