c2a446747bbbc60a9098439b504819360eb2ec52
Mikhail Panov <54293295+panovm@users.noreply.github.com>
2023-08-18 22:01:24 +0300

    solved prime_numbers.py

--- PrimeNumbers/prime_numbers.py
+++ PrimeNumbers/prime_numbers.py
@@ -11,8 +11,18 @@ def prime_numbers(n: int) -> List[int]:
     Returns:
         List[int]: prime numbers
     """
-
-    return []
+    if n < 2:
+        return 0
+    deleted = set()
+    prime = []
+    for i in range(2, n+1):
+        if i not in deleted:
+            prime.append(i)
+            j = 2
+            while j * i <= n:
+                deleted.add(j * i)
+                j += 1
+    return prime
 
 
 # https://leetcode.com/problems/count-primes/
